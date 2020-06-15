3218f9021f5ff74a517938394100be1cfd8133b0
ukiyo-roa <jnpco.98@gmail.com>
2020-06-15 10:25:47 +1000

    Added base monobehaviour singleton

---
+++ Assets/Scripts/Core/Utilities/Singleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System;
+
+namespace Ukiyo.Unity.Core.Utilities
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static readonly Lazy<T> lazyInstance = new Lazy<T>(CreateSingleton);
+
+        public static T Instance => lazyInstance.Value;
+
+        private static T CreateSingleton()
+        {
+            var gameobject = new GameObject($"{typeof(T).Name} (singleton)");
+            var instance = gameobject.AddComponent<T>();
+            DontDestroyOnLoad(gameobject);
+            return instance;
+        }
+    }
+}
