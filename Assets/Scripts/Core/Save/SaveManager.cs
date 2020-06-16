5676c0bceb421091887d203b6fed10bdd78e0751
ukiyo-roa <jnpco.98@gmail.com>
2020-06-16 22:35:34 +1000

    Refactored serialization. Moved out game logic to save manager.

---
+++ Assets/Scripts/Core/Save/SaveManager.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityCore.Utilities.Serialization;
+
+public class SaveManager
+{
+    public static readonly string SaveBasePath = Path.Combine(Application.persistentDataPath, "saves");
+    public static readonly string SaveFileExtension = ".sav";
+    
+    public List<string> SaveFiles { get { return GetSaveFiles(); } }
+
+    private SerializationManager _serializationManager;
+
+    public SaveManager()
+    {
+        _serializationManager = new SerializationManager();
+    }
+    
+    private List<string> GetSaveFiles()
+    {
+        if(!Directory.Exists(SaveBasePath))
+        {
+            Directory.CreateDirectory(SaveBasePath);
+        }
+
+        return Directory.GetFiles(SaveBasePath)
+            .Where(path => path.EndsWith(SaveFileExtension))
+            .Select(path => Path.GetFileNameWithoutExtension(path))
+            .ToList();
+    }
+
+    public void Save(string name, SaveData data)
+    {
+        _serializationManager.Save(SaveBasePath, name, data);
+    }
+
+    public SaveData Load(string name)
+    {
+        if(!SaveFiles.Contains(name)) return null;
+
+        var saveData = (SaveData)_serializationManager.Load(Path.Combine(SaveBasePath, name));
+        return saveData;
+    }
+}
