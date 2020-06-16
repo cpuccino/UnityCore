3dfe49d1a262181db69335bbddba3ec8da0db6b3
cpuccino <thegreatcappuccino@gmail.com>
2020-06-16 21:53:57 +1000

    Added base save data. Some scene updates.

---
+++ Assets/Scripts/Core/Serialization/SaveData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UnityCore.Utilities.Serialization
+{
+    [Serializable]
+    public class SaveData
+    {
+        private static object _singletonLock = new object();
+        private static SaveData _instance;
+        public static SaveData Current
+        {
+            get
+            {
+                lock(_singletonLock)
+                {
+                    if(_instance != null) return _instance;
+                    _instance = new SaveData();
+                    return _instance;
+                }
+            }
+        }
+
+        // Initialize values in constructor
+        // Replace Current with the appropriate savedata on game load
+        private SaveData() {}
+
+        // Add Save data properties
+    }
+}
