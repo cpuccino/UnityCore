386369c9ead040c3122cc4790bf0bbf6334a75ea
cpuccino <thegreatcappuccino@gmail.com>
2020-06-16 14:58:04 +1000

    Added functionality to the scene controller

--- Assets/Scripts/Core/Scene/SceneController.cs
+++ Assets/Scripts/Core/Scene/SceneController.cs
@@ -1,18 +1,119 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityCore.Page;
+using UnityCore.Utilities;
+using UnityCore.Audio;
+using System.Threading.Tasks;
 
-public class SceneController : MonoBehaviour
+namespace UnityCore.Scene
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SceneController : Singleton<SceneController>
     {
-        
-    }
+        PageController pageController;
+        AudioController audioController;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        PageType targetPageType;
+        SceneType targetSceneType;
+        bool sceneIsLoading;
+
+        Action<SceneType> onSceneLoadedCallback;
+
+        void Awake()
+        {
+            Initialize();
+            DontDestroyOnLoad(gameObject);
+        }
+
+        void Initialize()
+        {  
+            pageController = PageController.Instance;
+            audioController = AudioController.Instance;
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        async void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, LoadSceneMode mode)
+        {
+            if(targetSceneType == SceneType.None) return;
+
+            var sceneType = (SceneType)Enum.Parse(typeof(SceneType), scene.name);
+            if(targetSceneType != sceneType) return;
+
+            if(onSceneLoadedCallback != null)
+            {
+                try 
+                {
+                    onSceneLoadedCallback(sceneType);
+                } 
+                catch(Exception e)
+                {
+                    Debug.LogWarning($"Unable to run the callback after [{sceneType.ToString()}] was loaded. {System.Environment.NewLine}{e.Message}");
+                }
+            }
+
+            await Task.Delay(1000);
+
+            pageController.HidePage(targetPageType);
+            sceneIsLoading = false;
+        }
+
+        IEnumerator LoadScene()
+        {
+            pageController.ShowPage(targetPageType);
+            if(targetPageType != PageType.None)
+            {
+                while(!pageController.IsPageActive(targetPageType))
+                {
+                    yield return null;
+                }
+            }
+            Debug.Log("Attemping to load scene "+targetSceneType.ToString());
+            SceneManager.LoadScene(targetSceneType.ToString());
+        }
+
+        bool SceneCanBeLoaded(SceneType scene, bool reload)
+        {
+            if(sceneIsLoading)
+            {
+                Debug.LogWarning($"Unable to load scene [{scene.ToString()}]. Another scene [{targetSceneType.ToString()}] is in progress.");
+                return false;
+            }
+            if(!SceneManager.GetSceneByName(scene.ToString()).IsValid())
+            {
+                Debug.LogWarning($"Invalid scene name [{scene.ToString()}]");
+                return false;
+            }
+            if(SceneManager.GetActiveScene().name == scene.ToString() && !reload)
+            {
+                Debug.LogWarning($"You are trying to load a scene [{scene.ToString()}] that's currently active");
+                return false;
+            }
+
+            return true;
+        }
+
+        void OnDisable()
+        {
+            Dispose();
+        }
+
+        void Dispose()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        public void Load(SceneType targetSceneType, Action<SceneType> onSceneLoadedCallback = null, bool reload = false, PageType loadingPageType = PageType.None)
+        {
+            if(loadingPageType != PageType.None && pageController == null) return;
+            if(!SceneCanBeLoaded(targetSceneType, reload)) return;
+
+            sceneIsLoading = true;
+            this.targetSceneType = targetSceneType;
+            this.targetPageType = loadingPageType;
+            this.onSceneLoadedCallback = onSceneLoadedCallback;
+
+            StartCoroutine(LoadScene());
+        }
     }
 }
