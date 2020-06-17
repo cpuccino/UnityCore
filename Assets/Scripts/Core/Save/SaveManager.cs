72b44dcfe5b82b14a39aff2658f3efcc8e36daf1
cpuccino <thegreatcappuccino@gmail.com>
2020-06-17 12:23:57 +1000

    Renamed serialization classes

--- Assets/Scripts/Core/Save/SaveManager.cs
+++ Assets/Scripts/Core/Save/SaveManager.cs
@@ -11,13 +11,6 @@ public class SaveManager
 
     public List<string> SaveFiles { get { return GetSaveFiles(); } }
 
-    private SerializationManager _serializationManager;
-
-    public SaveManager()
-    {
-        _serializationManager = new SerializationManager();
-    }
-
     private List<string> GetSaveFiles()
     {
         if (!Directory.Exists(SaveBasePath))
@@ -33,14 +26,14 @@ public class SaveManager
 
     public void Save(string name, SaveData data)
     {
-        _serializationManager.Save(SaveBasePath, name, data);
+        Serialization.Save(SaveBasePath, name, data);
     }
 
     public SaveData Load(string name)
     {
         if (!SaveFiles.Contains(name)) return null;
 
-        var saveData = (SaveData)_serializationManager.Load(Path.Combine(SaveBasePath, name));
+        var saveData = (SaveData)Serialization.Load(Path.Combine(SaveBasePath, name));
         return saveData;
     }
 }
